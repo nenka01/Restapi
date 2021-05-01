9fb36dd53e373fb7d522a0aee76df42449589990
AgustD <agusdwisaputra159@gmail.com>
2021-05-01 12:36:15 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,12 +7,24 @@ const app = express();
 const dir = process.cwd();
 const api = require("./routes/api");
 
+const options = {
+  dotfiles: "ignore",
+  etag: false,
+  extensions: ["html", "htm"],
+  index: false,
+  maxAge: "999999d",
+  redirect: false,
+  setHeaders: function (res, path, stat) {
+    res.set("x-timestamp", Date.now())
+  }
+}
+
 app.use(cors());
 app.use(cookie());
 app.use(express.json());
 app.use(logger("dev"));
 app.set("json spaces", 2);
-app.use(express.static("public"));
+app.use(express.static("public", options));
 app.use(favicon(dir + "/public/favicon.ico"));
 
 app.use(api);
