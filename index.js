75d6843c7695d5cfd750cf6c66ec0b15ffd575ce
AgustD <agusdwisaputra159@gmail.com>
2021-05-01 12:21:21 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,28 +7,16 @@ const app = express();
 const dir = process.cwd();
 const api = require("./routes/api");
 
-const options = {
-  dotfiles: 'ignore',
-  etag: false,
-  extensions: ['html', 'htm'],
-  index: false,
-  maxAge: '999999d',
-  redirect: false,
-  setHeaders: function (res, path, stat) {
-    res.set('x-timestamp', Date.now())
-  }
-}
-
 app.use(cors());
 app.use(cookie());
 app.use(express.json());
 app.use(logger("dev"));
 app.set("json spaces", 2);
-app.use(express.static('public', options));
+app.use(express.static("public"));
 app.use(favicon(dir + "/public/favicon.ico"));
 
 app.use(api);
 
-app.listen(8080, () => {
-  console.log("Server berjalan di port 8080");
+app.listen(1234, () => {
+  console.log("Server berjalan di port 1234");
 });
